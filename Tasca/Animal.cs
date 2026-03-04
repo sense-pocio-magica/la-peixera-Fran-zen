07f151055a7edafc396f4fb6205980d27a677770
Fran <181867666+Fran-zen@users.noreply.github.com>
2026-03-04 15:20:22 +0100

    Create abs class Animal for All

---
+++ Tasca/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace Tasca;
+
+public abstract class Animal : IMarins
+{
+    private (int x, int y) _posicioInical;
+    private int _direccio; //2 down, 4 left, 6 right, 8 up
+    private char _sexe;
+    private bool _viu = true;
+    public void Mou()
+    {
+        
+    }
+
+    public Animal((int x, int y) posicioInical, int direccio, char sexe)
+    {
+        _posicioInical = posicioInical;
+        _direccio = direccio;
+        _sexe = sexe;
+    }
+}
