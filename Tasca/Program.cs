f8d7b61b69570df94fd8dab8040ba10dd3f7e1c0
Fran <181867666+Fran-zen@users.noreply.github.com>
2026-03-10 15:18:49 +0100

    Put code in main for how it works

--- Tasca/Program.cs
+++ Tasca/Program.cs
@@ -4,6 +4,13 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] animals = [10,2,3,2];
+        Peixera peixera = new();
+        peixera.Inicia(animals);
+        for (var i = 1; i <= 100; i++)
+        {
+            peixera.Ronda();
+        }
+        //peixera.Resultat();
     }
 }
