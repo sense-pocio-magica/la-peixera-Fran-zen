ccefb467bf47f7f969260115a5f9d513ca621cd4
Fran <181867666+Fran-zen@users.noreply.github.com>
2026-03-10 15:19:50 +0100

    changed parms

--- Tasca/Tortuga.cs
+++ Tasca/Tortuga.cs
@@ -2,13 +2,13 @@ namespace Tasca;
 
 public class Tortuga : Reproductor
 {
-    protected (int x, int y) _PosicioInical;
+    public (int x, int y) PosicioInical { get; }
     protected int _direccio; //2 down, 4 left, 6 right, 8 up
     protected int _sexe;
 
     public Tortuga((int x, int y) posicioInical, int direccio, int sexe) : base(posicioInical, direccio, sexe)
     {
-        _PosicioInical = posicioInical;
+        PosicioInical = posicioInical;
         _direccio = direccio;
         _sexe = sexe;
     }
